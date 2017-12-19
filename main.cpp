e857c086506eb66242cbaa794d513b2c673478b4
JonathanS <mail@strfry.org>
2017-12-19 13:32:41 +0100

    set work directory to exe folder

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
-#include <QThread>
-
 #include <QOpenGLContext>
-
-#include <QAbstractListModel>
+#include <QLibraryInfo>
 
 extern "C" {
 #include <re.h>
@@ -15,11 +12,11 @@ extern "C" {
 
 #include "baresipcore.h"
 #include "baresipvidisp.h"
-
 #include "contactlistmodel.h"
 #include "useragent.h"
 
-#include <QAbstractAnimation>
+#include <unistd.h>
+#include <libgen.h>
 
 int main(int argc, char *argv[])
 {
@@ -35,30 +32,28 @@ int main(int argc, char *argv[])
 	QGuiApplication app(argc, argv);
 
 	if (!QOpenGLContext::supportsThreadedOpenGL()) {
-		qInfo("Platform has no GL threading support!!!");
+		qWarning("Platform has no GL threading support!!!");
 		//exit(1);
 	}
 
 	BaresipCore::instance().start();
+	ContactListModel clModel;
+	
 	QQmlApplicationEngine engine;
 
-	ContactListModel clModel;
-
-	//qmlRegisterInterface<Call>("Call");
 	qRegisterMetaType<Call>();
 
+	// two mean hacks to get data inside QML
 	engine.rootContext()->setContextProperty("contactListModel", &clModel);
 	engine.rootContext()->setContextProperty("baresipCore", &BaresipCore::instance());
 
-	//qmlRegisterType<BaresipCore>("org.ewindow", 0, 1, "BaresipCore");
 	qmlRegisterInterface<BaresipCore>("BaresipCore");
-
-	//qmlRegisterSingletonType<BaresipThread>("org.ewindow.phone", 0, 1, "PhoneBackend", baresipthread_singleton_provider);
 	qmlRegisterSingletonType<UserAgent>("org.ewindow.ua", 0, 1, "UserAgent", &UserAgent::newInstance);
-	//qmlRegisterType<UserAgent>("org.ewindow", 0, 1, "UserAgent");
 	qmlRegisterUncreatableType<BaresipVidisp>("org.ewindow", 0, 1, "VideoDisplay", "VideoDisplay is created by the backend, use the onNewVideo callback");
 	qmlRegisterUncreatableType<ContactListModel>("org.ewindow", 0, 1, "ContactListModel", "ContactListModel is passed by the application through 'contactListModel'");
 
+	// Change working dir to exe's dir, so it will find the .qml's
+	chdir(dirname(argv[0]));
 	engine.load(QUrl(QStringLiteral("gui/main.qml")));
 	if (engine.rootObjects().isEmpty())
 		return -1;
