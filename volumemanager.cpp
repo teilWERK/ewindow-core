ea1fddb5803bd1f1e5643f704601e534356cebe4
JonathanS <mail@strfry.org>
2018-04-17 22:27:05 +0200

    Remove unnecessary files from baresip branch...

--- volumemanager.cpp
+++
@@ -1,8 +0,0 @@
-#include "volumemanager.h"
-
-void VolumeManager::setVolume(int percent)
-{
-    std::stringstream cmd;
-    cmd << "amixer set Master " << percent << "%";
-    system(cmd.str().c_str());
-}
